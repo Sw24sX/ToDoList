d4b38e93b33d805c1bd5622fc7b7bdc949ca42f4
sw24sx <aleksandr.korolyov.99@mail.ru>
2024-09-09 22:36:40 +0500

    Init project

---
+++ src/test/java/ru/sw24sx/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sw24sx.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
